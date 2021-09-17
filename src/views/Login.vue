01ff1754607aa6134c4e2af086e468da2e73f4b3
imaeda@X13 <dis.ma.magic1122@gmail.com>
2021-09-17 18:54:53 +0900

    passport無理

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,19 @@
 <template>
     <v-app>
-        <v-container fluid>
-            <v-row justify="center">
-                <v-col cols="12" md="9">
-                        <v-row class="ma-1">
-                            aaaa
-                        </v-row>
-                </v-col>
-            </v-row>
-        </v-container>
+        <v-card width="400px" class="mx-auto mt-5">
+            <v-card-title>
+            <h1 class="display-1">ログイン</h1>
+            </v-card-title>
+            <v-card-text>
+            <v-form>
+                <v-text-field prepend-icon="mdi-account-circle" v-model="loginId" label="ユーザ名" />
+                <v-text-field prepend-icon="mdi-lock" type="password" label="パスワード" v-model="password"/>
+            </v-form>
+            </v-card-text>
+            <v-card-actions>
+                <v-btn @click="auth">ログイン</v-btn>
+            </v-card-actions>            
+        </v-card>
     </v-app>
 </template>
 
@@ -22,10 +27,8 @@
     data () {
         return {
             debug: false,
-            isCreateSchedule:false,
-            isEditSchedule:false,
-            tmpSchedule:{},
-            schedules: []
+            loginId:'',
+            password: ''
         }
     },
     props: {
@@ -34,6 +37,19 @@
     computed:{
     },
     methods: {
+        auth: function(){
+            console.log('Auth')
+            this.axios.post('http://localhost:3000/api/auth',{
+                loginId: this.loginId,
+                password: this.password
+            })
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((e) => {
+                alert(e);
+            })
+        }
 
     },
     mounted() {
